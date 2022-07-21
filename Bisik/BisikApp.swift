3821e4d3afa6792281db5da3b3c3b015a969f6e6
Kevin Sam Andaria <Sam72725@gmail.com>
2022-07-21 16:14:48 +0700

    Initial Commit

---
+++ Bisik/BisikApp.swift
@@ -0,0 +1,17 @@
+//
+//  BisikApp.swift
+//  Bisik
+//
+//  Created by Kevin  Sam Andaria on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BisikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bisik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bisik
+//
+//  Created by Kevin  Sam Andaria on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
